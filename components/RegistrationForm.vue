da4d459f87e9881e07ec02f2262e2d7b498be4a7
Vincent Hall <jackvhall@outlook.com>
2020-06-06 11:03:11 -0700

    added logging for registration debug

--- components/RegistrationForm.vue
+++ components/RegistrationForm.vue
@@ -110,6 +110,7 @@ export default {
         this.response = res.data
         this.message = res.data.message
         this.status = res.status
+        console.log(res.data)
       } catch (e) {
         this.message = e.message
       }
