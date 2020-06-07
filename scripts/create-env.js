4e75e011dadc748351123243149c296a66a784d7
Vincent Hall <jackvhall@outlook.com>
2020-06-06 22:26:57 -0700

    fix writeFileSync override

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,15 +1,6 @@
 const fs = require('fs')
-
 fs.writeFileSync(
   './.env',
-  `PASSPORT_PASSWORD_GRANT_ID=${process.env.PASSPORT_PASSWORD_GRANT_ID}\n`
+  `PASSPORT_PASSWORD_GRANT_ID=${process.env.PASSPORT_PASSWORD_GRANT_ID}\n
+  PASSPORT_PASSWORD_GRANT_SECRET=${process.env.PASSPORT_PASSWORD_GRANT_SECRET}\n`
 )
-fs.writeFileSync(
-  './.env',
-  `PASSPORT_PASSWORD_GRANT_SECRET=${process.env.PASSPORT_PASSWORD_GRANT_SECRET}\n`
-)
-
-console.log(process.env.PASSPORT_PASSWORD_GRANT_ID)
-console.log(process.env.PASSPORT_PASSWORD_GRANT_SECRET)
-
-console.log(fs.readFileSync('./.env').toString('utf8'))
